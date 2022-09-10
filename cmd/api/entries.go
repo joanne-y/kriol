771a72dddee0f204ace8b028bbf61f1942b0b7ff
Joanne Yong <2019120152@ub.edu.bz>
2022-09-10 09:44:23 -0700

    moved the code that checks for the id parameter into its own method 'readIDParam'

--- cmd/api/entries.go
+++ cmd/api/entries.go
@@ -5,9 +5,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // createSchoolHandler for the "POST /v1/entries" endpoint
@@ -18,13 +15,11 @@ func (app *application) createEntryHandler(w http.ResponseWriter, r *http.Reques
 // showSchoolHnadler for the "GET v1/entries/:id" endpoint
 func (app *application) showEntryHandler(w http.ResponseWriter, r *http.Request) {
 	// Use the "ParamsFromContext()" function to get the request context as a slice
-	params := httprouter.ParamsFromContext(r.Context())
-	//Get the value of the "id" parameter
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	// Display the school id
+	// Display the entry id
 	fmt.Fprintf(w, "show the details for entry %d\n", id)
 }
